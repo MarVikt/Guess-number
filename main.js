8561ba57370f2420060955a6eee2144b8fc6cb6a
MarVikt <v.m.podporogie@gmail.com>
2022-03-31 19:11:29 +0300

    done with logs

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 function guessNumber(myNumber) {
   function askUser() {
-    let userNumber = +prompt('Загадано целое число от 1 до 100. Угадайте его!');
-    if (isNaN(parseInt(userNumber)) || !isFinite(userNumber) || userNumber <= 0 || userNumber > 100) {
-      if (userNumber < myNumber) {
-        alert('Загаданное число больше');
-        askUser();
-      } else if (userNumber > myNumber) {
-        alert('Загаданное число меньше');
-        askUser();
+    let userNumber = prompt("Загадано целое число от 1 до 100. Угадайте его!");
+    // console.log(userNumber + " type " + typeof userNumber);
+    if (userNumber === null) {
+      alert("Игра окончена");
+    } else if (isNaN(userNumber) || userNumber.trim() === "") {
+      alert("Введите число!");
+      askUser();
+    } else {
+      userNumber = Number(userNumber);
+      // console.log(userNumber + " type now " + typeof userNumber);
+      if (userNumber >= 1 && userNumber <= 100) {
+        if (userNumber < myNumber) {
+          alert("Загаданное число больше");
+          askUser();
+        } else if (userNumber > myNumber) {
+          alert("Загаданное число меньше");
+          askUser();
+        } else {
+          alert("Поздравляю, Вы угадали!!!  Это число " + myNumber);
+        }
       } else {
-        alert('Поздравляю, Вы угадали!!!');
+        alert('Число должно быть от 1 до 100');
+        askUser();
       }
     }
   }
@@ -17,5 +30,3 @@ function guessNumber(myNumber) {
 }
 
 guessNumber(44);
-
-
